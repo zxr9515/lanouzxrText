f32b90daf76e72a392937568fde3a418f7698fa7
zxr9515 <1456431623@qq.com>
2016-05-25 15:51:56 +0800

    我新建了一个项目

---
+++ UI_zxrText/UI_zxrText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI_zxrText
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

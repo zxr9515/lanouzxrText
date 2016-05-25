aebaf631c23e0394b9e69d11ce25d3d4f1ff5b12
zxr9515 <1456431623@qq.com>
2016-05-25 15:57:06 +0800

    改动了viewControlller,新添加了FirstviewControlller

---
+++ UI_zxrText/UI_zxrText/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  UI_zxrText
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
--- UI_zxrText/UI_zxrText/ViewController.m
+++ UI_zxrText/UI_zxrText/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int number = 5;
+    int sum = 100;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
